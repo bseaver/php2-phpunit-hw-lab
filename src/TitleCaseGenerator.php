a8b2934816807c6e866657e85dc4c706fcceb379
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-02-12 13:33:38 -0800

    Add code to class to pass second test

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -3,7 +3,12 @@
     {
         function makeTitleCase($input_title)
         {
-            return ucfirst($input_title);
+            $input_array_of_words = explode(" ", $input_title);
+            $output_title_cased = array();
+            foreach ($input_array_of_words as $word) {
+                array_push($output_title_cased, ucfirst($word));
+            }
+            return implode(" ", $output_title_cased);
         }
     }
 ?>
